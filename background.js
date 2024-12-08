1225cc07a5179a7efefb450b0da5e08e27509214
Design And Creation <romain.doyen97@gmail.com>
2024-12-08 12:30:08 +0400

    Creating folders (Other, Images, Videos, Audio, Documents)

--- background.js
+++ background.js
@@ -1,1 +1,16 @@
-console.log('background.js loaded');
+chrome.downloads.onDeterminingFilename.addEventListener((downloadItem, suggest) => {
+  let folder = "Autres";
+
+  if (downloadItem.mime.startsWith("image/")) {
+    folder = "Images";
+  } else if (downloadItem.mime.startsWith("video/")) {
+    folder = "Vidéos";
+  } else if (downloadItem.mime.startsWith("audio/")) {
+    folder = "Audios";
+  } else if (downloadItem.mime.startsWith("application/")) {
+    folder = "Documents";
+  }
+
+  const newFilename = `${folder}/${downloadItem.filename}`;
+  suggest({ filename: newFilename });
+});
